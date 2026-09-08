e7b6bcb0c631fa5d57e9e0069fd50c815ef7316e
sanadplatform2027-arch <sanadplatform2027@gmail.com>
2026-09-08 21:14:06 +0100

    Fix logo source path to public assets

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -1,5 +1,3 @@
-import logoAsset from "@/assets/sanad-logo.png.asset.json";
-
 export function BrandLogo({ className = "h-9 w-9", alt = "شعار منصة سند" }: { className?: string; alt?: string }) {
-  return <img src={logoAsset.url} alt={alt} className={`object-contain ${className}`} />;
+  return <img src="/sanad-logo.png" alt={alt} className={`object-contain ${className}`} />;
 }
